6f560b2d8dcfface7892034e5a1247d276695901
lecion <ylc931116@gmail.com>
2016-03-28 21:07:12 +0800

    Reply Model

---
+++ app/models/Reply.js
@@ -0,0 +1,23 @@
+/**
+ * Created by Lecion on 3/28/16.
+ */
+var mongoose    = require('mongoose');
+var Schema      = mongoose.Schema;
+var MongooseDao = require('mongoosedao');
+var ObjectId    = Schema.Types.ObjectId;
+var ReplySchema = new Schema({
+    content  : String,
+    goods_id : {type : ObjectId, ref : 'Goods'},
+    reply_id : {type : ObjectId, ref : 'Reply'},
+    from     : {type : ObjectId, ref : 'User'},
+    to       : {type : ObjectId, ref : 'User'},
+    deleted  : {type : Boolean, default : false},
+});
+
+ReplySchema.plugin(require('./BaseModel'));
+ReplySchema.index({goods_id : 1});
+ReplySchema.index({from : 1, create_at : -1});
+
+var ReplyModel = mongoose.model('Reply', ReplySchema);
+
+module.exports = new MongooseDao(ReplyModel);
